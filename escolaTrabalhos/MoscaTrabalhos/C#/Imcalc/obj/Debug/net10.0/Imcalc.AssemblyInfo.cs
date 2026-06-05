eb028fc2c788cf9343eaa48dbcde0d0f3f549223
Jelado1234 <fredericoteodoroarantes@gmail.com>
2026-06-05 08:49:10 -0300

    woaw

--- escolaTrabalhos/MoscaTrabalhos/C#/Imcalc/obj/Debug/net10.0/Imcalc.AssemblyInfo.cs
+++ escolaTrabalhos/MoscaTrabalhos/C#/Imcalc/obj/Debug/net10.0/Imcalc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Imcalc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7463399559fd2f198b0ec4ecea75acf54b58fd51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7fe9c3522b8d060594c90edf2a15ab179f1915e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Imcalc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Imcalc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
