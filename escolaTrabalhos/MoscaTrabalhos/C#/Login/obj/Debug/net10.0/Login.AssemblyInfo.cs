fab589665c23158c75903d53a4ed4f6b4ada14f4
Juremo-Pinto <fredericoteodoroarantes@gmail.com>
2026-06-17 10:15:23 -0300

    nothing

--- escolaTrabalhos/MoscaTrabalhos/C#/Login/obj/Debug/net10.0/Login.AssemblyInfo.cs
+++ escolaTrabalhos/MoscaTrabalhos/C#/Login/obj/Debug/net10.0/Login.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Login")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0aaecf01c4043c015a180f794bb07b381eda2a84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb40564291078b06d44a7093f222e4f79cd582b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Login")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Login")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
