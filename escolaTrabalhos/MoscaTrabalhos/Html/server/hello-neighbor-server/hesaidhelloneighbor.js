87a67f3227610fe4630b46fe012f719eaac0fadf
Jelado1234 <fredericoteodoroarantes@gmail.com>
2026-06-16 22:12:35 -0300

    server :D

--- escolaTrabalhos/MoscaTrabalhos/Html/server/hello-neighbor-server/hesaidhelloneighbor.js
+++ escolaTrabalhos/MoscaTrabalhos/Html/server/hello-neighbor-server/hesaidhelloneighbor.js
@@ -39,18 +39,19 @@ const server = http.createServer((req, res) => {
     } else if (['/oi', '/ola', '/hi'].includes(req.url)) {
         res.writeHead(301, { 'Location': '/hello' });
         res.end();
-        
+
     } else {
-    res.writeHead(404, {
-        'Content-Type': 'text/html'
-    });
+        const filePath = path.join(__dirname, 'index404.html');
 
-    res.end('<h1>404 - Not Found</h1>');
-}
+        fs.readFile(filePath, (err, content) => {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.end(content);
+        });
+    }
+});
 
-})
 const port = 3001;
 server.listen(port, () => {
-    console.log('Servidor rodano em http://localhost:${port}/');
+    console.log('Servidor rodano em http://localhost:3001/');
 
 });
